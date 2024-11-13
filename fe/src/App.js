23de3be9dfaa65fbba27664d57bb38d40e70dbd6
Liron Avital <warrockla@gmail.com>
2024-11-13 04:32:19 +0200

    - localdb with commands (no salting) - login [http(s) only cookie] - jwt basics - basic reactiveness - Login to FB saves ACT & Expire

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,17 +1,45 @@
 import { Routes, Route, Link, useParams } from "react-router-dom";
-import Main from "./Pages/Main";
-import { useState } from "react";
-import Login from "./Pages/Login";
+import Main from "./pages/Main";
+import { useEffect, useState } from "react";
+import Login from "./pages/Login";
+import { getConfig } from "./config/getConfig";
+import axios from "axios";
+const config = getConfig()
+
+axios.defaults.withCredentials = true;
 
 function App() {
-  const [loggedUser, setLoggedUser] = useState({ logged: false, key: "" })
+  const [loggedUser, setLoggedUser] = useState(false)
+
+  async function getLoginStatusFromCookie() {
+    try {
+      const loginResp = await axios.get(`${config.backend}/login/status`)
+      if (loginResp.status === 200) {
+        setLoggedUser(true)
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        console.log("User isn't logged in.")
+      }
+    }
+  }
+
+  async function logout() {
+    await axios.delete(`${config.backend}/login`)
+    setLoggedUser(false)
+  }
+
+
+  useEffect(() => {
+    getLoginStatusFromCookie()
+  })
+
 
   return (
     <div className="App">
-
+      {loggedUser && <button onClick={logout}>Log Out</button>}
       <Routes>
-        <Route path="/" element={<Main />} />
-        {/* {loggedUser.logged ? <Route path="/hls/admin/aaa" element={<Admin loggedUser={loggedUser} />} /> : <Route path="/hls/admin/aaa" element={<Login setLoggedUser={setLoggedUser} />} />} */}
+        {loggedUser ? <Route path="/" element={<Main loggedUser={loggedUser} />} /> : <Route path="/" element={<Login setLoggedUser={setLoggedUser} />} />}
       </Routes>
     </div>
   );
--- fe/src/Pages/Main.js
+++ fe/src/Pages/Main.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react'
 import '../main.css'
 import axios from 'axios'
 import { getConfig } from '../config/getConfig'
+import LoginToPlatforms from '../components/LoginToPlatforms'
 const config = getConfig()
 
-export default function Main() {
+export default function Main({ }) {
     return (<div>
         HI
+        <LoginToPlatforms />
     </div>)
 }
